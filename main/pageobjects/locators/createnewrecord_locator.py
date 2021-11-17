e6bc754e55ec1be45665360ab48f1e382dda1257
Yetendra <sandeep.yetendra@gmail.com>
2021-11-17 22:51:26 +0530

    Update createnewrecord_locator.py

--- main/pageobjects/locators/createnewrecord_locator.py
+++ main/pageobjects/locators/createnewrecord_locator.py
@@ -1,37 +1,75 @@
 """
-NEW RECORD PAGE
-LOCATOR PLACE HOLDER
+PAGE OBJECT
 """
-FIRST_NAME = "xpath,/html/body/div[1]/div/div/ui-view/section/div[1]/form/div/div[1]/div/div/div[2]/div[1]/div[" \
-            "1]/div/div/div[1]/div/div/div/input "
-LAST_NAME = "xpath,/html/body/div[1]/div/div/ui-view/section/div[1]/form/div/div[1]/div/div/div[2]/div[1]/div[" \
-           "2]/div/div/div[1]/div/div/div/input "
-STREET_ADDRESS = "xpath,/html/body/div[1]/div/div/ui-view/section/div[1]/form/div/div[1]/div/div/div[2]/div[2]/div[" \
-               "1]/div/div/div[1]/div/div/div/textarea "
-CITY = (
-    "xpath,/html/body/div[1]/div/div/ui-view/section/div[1]/form/div/div[1]/div/div/div[2]/div[2]/div["
-    "2]/div/div/div[1]/div/div/div/input "
-)
-STATE = (
-    "xpath,/html/body/div[1]/div/div/ui-view/section/div[1]/form/div/div[1]/div/div/div[2]/div[3]/div["
-    "1]/div/div/div[1]/div/div/div/input "
-)
-TELEPHONE = (
-    "xpath,/html/body/div[1]/div/div/ui-view/section/div[1]/form/div/div[1]/div/div/div[2]/div[3]/div["
-    "2]/div/div/div[1]/div/div/div/input "
-)
-ZIPCODE = (
-    "xpath,/html/body/div[1]/div/div/ui-view/section/div[1]/form/div/div[1]/div/div/div[2]/div[4]/div[1]/div/div/div["
-    "1]/div/div/input "
-)
-EMAIL = (
-    "xpath,/html/body/div[1]/div/div/ui-view/section/div[1]/form/div/div[1]/div/div/div[2]/div[4]/div["
-    "2]/div/div/div[1]/div/div/div/input "
-)
-TEXT = "xpath,/html/body/div[1]/div/div/ui-view/section/div[1]/form/div/div[2]/div/div/div/div[1]/div/div/div/input"
-SAVE_BUTTON = "xpath,/html/body/div[1]/div/div/ui-view/div/div/div[2]/toolbar/ul/li[1]/button/span"
-SAVE_TIME_SPENTBUTTON = "xpath,/html/body/div[5]/div/div/div[2]/button"
-CREATE_NEWRECORD_BUTTON = (
-    "xpath,/html/body/div[1]/navigation-bar/ngx-nav-menu/div/ul/li[3]/div["
-    "2]/nav-filter-list/div/ul/li/div/a/ngx-icon/i "
-)
+from pageobjects.locators import createnewrecord_locator
+from utility.basepage import BasePage
+
+
+class CreateRecordpage(BasePage):
+    """
+    Page object for new record creation
+    """
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def set_firstname(self, firstname):
+        """
+        sets first name
+        :param firstname: string
+        :return: void
+        """
+        self.get_web_element(createnewrecord_locator.FIRST_NAME,self.driver).clear()
+        self.get_web_element(createnewrecord_locator.FIRST_NAME,self.driver). \
+            send_keys(firstname)
+
+    def set_lastname(self, lastname):
+        """
+        sets last name
+        :param lastname:string
+        :return: void
+        """
+        self.get_web_element(createnewrecord_locator.LAST_NAME,self.driver).clear()
+        self.get_web_element(createnewrecord_locator.LAST_NAME,self.driver). \
+            send_keys(lastname)
+
+    def set_city(self, city):
+        """
+        sets text city
+        :param city: string
+        :return: void
+        """
+        self.get_web_element(createnewrecord_locator.CITY,self.driver).clear()
+        self.get_web_element(createnewrecord_locator.CITY,self.driver). \
+            send_keys(city)
+
+    def click_savebutton(self):
+        """
+        clicks on save button
+        :return: void
+        """
+        self.get_web_element(createnewrecord_locator.SAVE_BUTTON,self.driver). \
+            click()
+
+    def click_savetimespent(self):
+        """
+        clicks on button
+        """
+        self.get_web_element(createnewrecord_locator.SAVE_TIME_SPENTBUTTON,self.driver). \
+            click()
+
+    def goto_newrecord_page(self):
+        """
+        Navigate to new record craetion page
+        :return: void
+        """
+        self.get_web_element(createnewrecord_locator.CREATE_NEWRECORD_BUTTON,self.driver). \
+            click()
+
+    def is_savebutton_enabled(self):
+        """
+        checks for existance
+        :return: boolean
+        """
+        return self.get_web_element(createnewrecord_locator.SAVE_BUTTON,self.driver). \
+            is_displayed()
